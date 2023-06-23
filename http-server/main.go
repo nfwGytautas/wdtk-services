70d32b9a4fc23d67513f399fd8a8f8f1fdac39e1
Gytautas Kazlauskas <gytautas.kk@gmail.com>
2023-06-23 12:08:03 +0300

    Updated with config for htmlDirectory

--- http-server/main.go
+++ http-server/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"log"
+	"path/filepath"
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
+	"github.com/nfwGytautas/gdev/file"
 	"github.com/nfwGytautas/wdtk-go-backend/microservice"
 )
 
@@ -16,8 +18,25 @@ func main() {
 		panic(err)
 	}
 
+	htmlDir, exists := config.UserDefines["htmlDirectory"]
+	if !exists {
+		log.Println("Missing 'htmlDirectory' config")
+		return
+	}
+
+	if !file.Exists(htmlDir.(string)) {
+		log.Println("The directory '" + htmlDir.(string) + "' doesn't exist")
+		return
+	}
+
+	abs, err := filepath.Abs(htmlDir.(string))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
 	r := gin.Default()
 
-	r.Use(static.Serve("/", static.LocalFile("./", true)))
+	r.Use(static.Serve("/", static.LocalFile(abs, true)))
 	r.Run(config.RunAddress)
 }
