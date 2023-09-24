d5e99d5e9c4b573b76b31d820b02014be2b46030
Gytautas Kazlauskas <gytautas.kk@gmail.com>
2023-09-24 18:54:43 +0300

    HTTPS for server

--- http-server/main.go
+++ http-server/main.go
@@ -38,5 +38,21 @@ func main() {
 	r := gin.Default()
 
 	r.Use(static.Serve("/", static.LocalFile(abs, true)))
-	r.Run(config.RunAddress)
+
+	httpsEnabled, httpsDefined := config.UserDefines["HTTPS"]
+	if httpsDefined && httpsEnabled.(bool) {
+		// HTTPS requires key and certification file
+		certFile, certDefined := config.UserDefines["CertFile"]
+		keyFile, keyDefined := config.UserDefines["KeyFile"]
+
+		if !certDefined || !keyDefined {
+			log.Fatal("'CertFile' and 'KeyFile' must be defined")
+			return
+		}
+
+		r.RunTLS(config.RunAddress, certFile.(string), keyFile.(string))
+	} else {
+		// HTTP
+		r.Run(config.RunAddress)
+	}
 }
